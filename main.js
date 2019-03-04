94bc5476a9e1fca173ac6b3c7af2ed6a51b2d6a1
Kuba <ja.tanski@gmail.com>
2019-03-04 20:12:58 +0100

    Zmiana nava po scrollu i dalsze prace przy about

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const icoBurger = document.querySelector('.burger');
 const icoClose = document.querySelector('.close');
 const mobileUl = document.querySelector('ul');
+const ulContainer = document.querySelector('.ulContainer')
 const header = document.querySelector('header')
 
 icoBurger.addEventListener('click', function () {
@@ -15,4 +16,16 @@ icoClose.addEventListener('click', function () {
     icoBurger.classList.remove('show');
     mobileUl.classList.add('show');
     header.style.minHeight = "90vh"
+})
+
+window.addEventListener('scroll', function () {
+    console.log(this.scrollY)
+    console.log(header.offsetHeight)
+    if (this.scrollY > header.offsetHeight - ulContainer.offsetHeight) {
+        ulContainer.classList.add('menuOpacity');
+    } else {
+        ulContainer.classList.remove('menuOpacity');
+    }
+
+
 })
